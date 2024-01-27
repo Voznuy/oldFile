e8a9a1bce48b413f4e4e02ccf99227750e3fe724
Voznuy <voznijartem@gmail.com>
2024-01-27 12:05:09 +0200

    01

--- main.py
+++ main.py
@@ -1,35 +1,21 @@
 import os
-import datetime
+import time
 
 
-def remove_old_files_and_empty_folders(directory_path, days_threshold=10):
-    current_time = datetime.datetime.now()
-    threshold = current_time - datetime.timedelta(days=days_threshold)
+def delete_old_files(directory, days=15):
+    current_time = time.time()
+
+    for item in os.listdir(directory):
+        item_path = os.path.join(directory, item)
+
+        if os.path.isfile(item_path):
+            file_mtime = os.path.getmtime(item_path)
+            time_difference = current_time - file_mtime
+            if time_difference > days * 24 * 3600:
+                os.remove(item_path)
+                print(f"Видалено файл: {item_path}")
+        elif os.path.isdir(item_path):
+            delete_old_files(item_path, days)
 
 
-    def remove_empty_folders(folder):
-        try:
-            for item in os.listdir(folder):
-                item_path = os.path.join(folder, item)
-                if os.path.isdir(item_path):
-                    remove_empty_folders(item_path)
-            if not os.listdir(folder):
-                os.rmdir(folder)
-                print(f"Folder {folder} deleted")
-        except Exception as e:
-            print(f"Error delete folder {folder}: {e}")
-    for filename in os.listdir(directory_path):
-        file_path = os.path.join(directory_path, filename)
-        if os.path.isfile(file_path):
-            last_modified_time = datetime.datetime.fromtimestamp(os.path.getmtime(file_path))
-            if last_modified_time < threshold:
-                try:
-                    os.remove(file_path)
-                    print(f"File {filename}deleted")
-                except Exception as e:
-                    print(f"Error deleted file {filename}: {e}")
-    remove_empty_folders(directory_path)
-
-
-directory_path = "/home/admin228a/Videos"
-remove_old_files_and_empty_folders(directory_path)
+delete_old_files('/home/admin228a/Videos', days=15)
